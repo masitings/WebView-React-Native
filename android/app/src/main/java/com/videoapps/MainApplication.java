1c69187e9666c20ce9fdac2adc14d95f6deab392
Rafi Halilintar <masitingss@gmail.com>
2020-05-02 23:18:53 +0700

    Adding Admob meta

--- android/app/src/main/java/com/videoapps/MainApplication.java
+++ android/app/src/main/java/com/videoapps/MainApplication.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.sbugert.rnadmob.RNAdMobPackage;
 import com.reactnativecommunity.webview.RNCWebViewPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.facebook.react.ReactNativeHost;
